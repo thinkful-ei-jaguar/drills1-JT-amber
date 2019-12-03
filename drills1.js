2812af537b7a97394f7de09916d5e91c9aed7183
JTLiner925 <55215116+JTLiner925@users.noreply.github.com>
2019-12-02 20:40:30 -0600

    Add files via upload

--- drills1.js
+++ drills1.js
@@ -2,7 +2,8 @@
 /* eslint-disable strict */
 
 function createGreeting(name,age){   
-
+  if(!age || !name || isNaN(parseFloat(age))) 
+    throw new Error("Arguments not valid");
   return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${getYearOfBirth(age)}.`;
 }
 function getYearOfBirth(age){
@@ -12,7 +13,7 @@ function getYearOfBirth(age){
   return yearOfBirth;
 }
 try { 
-  const greeting1 = createGreeting('JT', -36);
+  const greeting1 = createGreeting('JT','twenty');
   console.log(greeting1);
 } catch (error) {
   console.log(error);
