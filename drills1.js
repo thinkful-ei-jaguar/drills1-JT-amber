81c75e209ea3a10612c2fed3fee2edf98d4a9a29
JT Liner <[jtliner925@gmail.com]>
2019-12-02 18:50:39 -0600

    function get YearOfBirth

--- drills1.js
+++ drills1.js
@@ -2,9 +2,14 @@
 /* eslint-disable strict */
 
 function createGreeting(name,age){   
-    const yearOfBirth = 2019 - age;
+   // const yearOfBirth = 2019 - age;
     return 'Hi, my name is ${name} and I\'m ${age} years old. I was born in ${yearOfBirth}.'
 }
-const greeting1 = createGreeting();
+function getYearOfBirth(age){
+    const yearOfBirth = 2019 - age;
+    return yearOfBirth;
+}
 
+const greeting1 = createGreeting();
 console.log(greeting1);
+console.log(getYearOfBirth);
