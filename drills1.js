0ed3c2980968b2319fc6092c2bb67d57cf1e0d75
JT Liner <[jtliner925@gmail.com]>
2019-12-02 18:34:50 -0600

    const yearOfBirth

--- drills1.js
+++ drills1.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-console */
 /* eslint-disable strict */
-function createGreeting(name,age){
-    return 'Hi, my name is ${name} and I\'m ${age} years old.'
-}
 
+function createGreeting(name,age){   
+    const yearOfBirth = 2019 - age;
+    return 'Hi, my name is ${name} and I\'m ${age} years old. I was born in ${yearOfBirth}.'
+}
 const greeting1 = createGreeting();
+
 console.log(greeting1);
