acece54634da9cd1073911e19c7147ee8ebedcb8
JT Liner <[jtliner925@gmail.com]>
2019-12-02 20:19:48 -0600

    Added try, catch, throw

--- drills1.js
+++ drills1.js
@@ -2,14 +2,20 @@
 /* eslint-disable strict */
 
 function createGreeting(name,age){   
-   // const yearOfBirth = 2019 - age;
-    return 'Hi, my name is ${name} and I\'m ${age} years old. I was born in ${yearOfBirth}.'
+
+  return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${getYearOfBirth(age)}.`;
 }
 function getYearOfBirth(age){
-    const yearOfBirth = 2019 - age;
-    return yearOfBirth;
+  if(age < 0)
+    throw new Error("Age can not be negative");
+  const yearOfBirth = 2019 - age;
+  return yearOfBirth;
 }
-
-const greeting1 = createGreeting();
-console.log(greeting1);
-console.log(getYearOfBirth);
+try { 
+  const greeting1 = createGreeting('JT', -36);
+  console.log(greeting1);
+} catch (error) {
+  console.log(error);
+}
+//console.log(greeting1);
+//console.log(getYearOfBirth());
